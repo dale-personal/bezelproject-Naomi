036ece0e8b6c7ef1f1c12c811ac6d2e3acfddfd6
Dale Koch <dalek@marvin.com>
2022-04-17 15:24:26 -0500

    script fix

--- rename-config-files.ps1
+++ rename-config-files.ps1
@@ -2,6 +2,6 @@ $files = Get-ChildItem -File -Filter *.cfg -Path retroarch\config\Flycast\
 
 for ($i=0; $i -lt $files.Count; $i++) {
     Write-Output $files[$i].FullName
-    $new_name = $files[$i].BaseName + ".7z" + $files[$i].Extension
+    $new_name = $files[$i].BaseName + ".zip" + $files[$i].Extension
     Rename-Item -Path $files[$i].FullName -NewName $new_name
 }
